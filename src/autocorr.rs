2520af6abc56fea45f28e687bf3f848f7dff1050
neithanmo <neithanmo@gmail.com>
2019-12-02 15:39:38 -0600

    using the new utils module

--- src/autocorr.rs
+++ src/autocorr.rs
@@ -1,37 +1,11 @@
 use std::fmt;
-
-use crate::liquid_dsp_sys as raw;
-use libc::{c_uint};
-
-use num::complex::{Complex32};
 use std::mem::transmute;
 
-pub(crate) trait ToRaw<T> {
-    unsafe fn to_raw(&mut self) -> *mut T;
-}
+use crate::liquid_dsp_sys as raw;
+use libc::c_uint;
+use num::complex::Complex32;
 
-pub(crate) type LiquidFloatComplex = raw::liquid_float_complex;
-
-#[derive(Debug, Clone)]
-pub(crate) struct LiquidComplex(LiquidFloatComplex);
-
-impl From<Complex32> for LiquidComplex {
-    fn from(value: Complex32) -> Self {
-        Self(LiquidFloatComplex {
-            re: value.re,
-            im: value.im,
-        })
-    }
-}
-
-impl From<LiquidComplex> for Complex32 {
-    fn from(value: LiquidComplex) -> Self {
-        Self {
-            re: value.0.re,
-            im: value.0.im,
-        }
-    }
-}
+use crate::utils::{LiquidComplex, LiquidFloatComplex};
 
 pub struct AutoCorrRrrf {
     inner: raw::autocorr_rrrf,
@@ -48,9 +22,7 @@ pub struct AutoCorrCccf {
 impl AutoCorrCccf {
     ///  creates and returns an autocorr object with a *window* size of N samples and a *delay* of d samples.
     pub fn create(N: u32, d: u32) -> Self {
-        let inner = unsafe {
-            raw::autocorr_cccf_create(N as c_uint, d as c_uint)
-        };
+        let inner = unsafe { raw::autocorr_cccf_create(N as c_uint, d as c_uint) };
         Self {
             inner,
             window: N,
@@ -76,25 +48,37 @@ impl AutoCorrCccf {
             let mut out = Complex32::default();
             let ptr = &mut out as *mut Complex32;
             // this is safe because Complex<T> reproduce c
-            raw::autocorr_cccf_execute(self.inner, transmute::<*mut Complex32, *mut LiquidFloatComplex>(ptr));
+            raw::autocorr_cccf_execute(
+                self.inner,
+                transmute::<*mut Complex32, *mut LiquidFloatComplex>(ptr),
+            );
             *ptr
         }
     }
 
     pub fn execute_block(&self, input: &[Complex32], output: &mut [Complex32]) {
-        assert!(input.len() == output.len(), "Input and output buffers with different length");
-        input.iter().zip(output.iter_mut()).for_each(|(isample, osample)| {
-            self.push(*isample);
-            unsafe {
-                raw::autocorr_cccf_execute(self.inner, transmute::<*mut Complex32, *mut LiquidFloatComplex>(osample as *mut Complex32));
-            }    
-        });
+        assert!(
+            input.len() == output.len(),
+            "Input and output buffers with different length"
+        );
+        input
+            .iter()
+            .zip(output.iter_mut())
+            .for_each(|(isample, osample)| {
+                self.push(*isample);
+                unsafe {
+                    raw::autocorr_cccf_execute(
+                        self.inner,
+                        transmute::<*mut Complex32, *mut LiquidFloatComplex>(
+                            osample as *mut Complex32,
+                        ),
+                    );
+                }
+            });
     }
 
     pub fn get_energy(&self) -> f32 {
-        unsafe {
-            raw::autocorr_cccf_get_energy(self.inner) as f32
-        }
+        unsafe { raw::autocorr_cccf_get_energy(self.inner) as f32 }
     }
 }
 
@@ -115,9 +99,7 @@ impl Drop for AutoCorrCccf {
 impl AutoCorrRrrf {
     ///  creates and returns an autocorr object with a *window* size of N samples and a *delay* of d samples.
     pub fn create(N: u32, d: u32) -> Self {
-        let inner = unsafe {
-            raw::autocorr_rrrf_create(N as c_uint, d as c_uint)
-        };
+        let inner = unsafe { raw::autocorr_rrrf_create(N as c_uint, d as c_uint) };
         Self {
             inner,
             window: N,
@@ -146,19 +128,23 @@ impl AutoCorrRrrf {
     }
 
     pub fn execute_block(&self, input: &[f32], output: &mut [f32]) {
-        assert!(input.len() == output.len(), "Input and output buffers with different length");
-        input.iter().zip(output.iter_mut()).for_each(|(isample, osample)| {
-            self.push(*isample);
-            unsafe {
-                raw::autocorr_rrrf_execute(self.inner, osample as *mut _);
-            }    
-        });
+        assert!(
+            input.len() == output.len(),
+            "Input and output buffers with different length"
+        );
+        input
+            .iter()
+            .zip(output.iter_mut())
+            .for_each(|(isample, osample)| {
+                self.push(*isample);
+                unsafe {
+                    raw::autocorr_rrrf_execute(self.inner, osample as *mut _);
+                }
+            });
     }
 
     pub fn get_energy(&self) -> f32 {
-        unsafe {
-            raw::autocorr_rrrf_get_energy(self.inner) as f32
-        }
+        unsafe { raw::autocorr_rrrf_get_energy(self.inner) as f32 }
     }
 }
 
@@ -178,14 +164,14 @@ impl Drop for AutoCorrRrrf {
 
 #[cfg(test)]
 mod tests {
-    use num::complex::{Complex32};
-    use num::Zero;
     use super::{AutoCorrCccf, AutoCorrRrrf};
+    use num::complex::Complex32;
+    use num::Zero;
 
     #[test]
     fn test_autocorr_cccf() {
         let auto_cccf = AutoCorrCccf::create(16, 8);
-        assert_eq!(&format!("{:?}", auto_cccf),"autocorr [16 window, 8 delay]");
+        assert_eq!(&format!("{:?}", auto_cccf), "autocorr [16 window, 8 delay]");
     }
 
     #[test]
@@ -197,14 +183,19 @@ mod tests {
         }
         let auto_cccf = AutoCorrCccf::create(4, 0);
         auto_cccf.execute_block(&input, &mut output);
-        let solution = [Complex32::new(20.25, 0.0), Complex32::new(51.50, 0.0), Complex32::new(97.75, 0.0), Complex32::new(163.0, 0.0)];
+        let solution = [
+            Complex32::new(20.25, 0.0),
+            Complex32::new(51.50, 0.0),
+            Complex32::new(97.75, 0.0),
+            Complex32::new(163.0, 0.0),
+        ];
         assert_eq!(&output, &solution);
     }
 
     #[test]
     fn test_autocorr_rrrf() {
         let auto_rrrf = AutoCorrRrrf::create(16, 8);
-        assert_eq!(&format!("{:?}", auto_rrrf),"autocorr [16 window, 8 delay]");
+        assert_eq!(&format!("{:?}", auto_rrrf), "autocorr [16 window, 8 delay]");
     }
 
     #[test]
@@ -221,4 +212,3 @@ mod tests {
         assert_eq!(&output, &solution);
     }
 }
-
