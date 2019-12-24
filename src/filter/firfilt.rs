3f76213561f837e9d60cc90d3d7fdafd9c684e9f
neithanmo <neithanmo@gmail.com>
2019-12-24 07:31:01 -0600

    more functions

--- src/filter/firfilt.rs
+++ src/filter/firfilt.rs
@@ -20,19 +20,50 @@ pub struct FirFiltCccf {
 }
 
 macro_rules! firfilt_impl {
-    ($obj:ty, ($create_prototype:expr,
-        $create_lowpass:expr,
-        $create_integrator:expr,
-        $create_differentiator:expr,
-        $create_dc_blocker:expr, 
-        $create_pll:expr,
+    ($obj:ty, ($reset:expr,
         $print:expr,
-        $reset:expr, 
-        $len:expr, 
+        $glen:expr,
         $freq_response:expr,
         $group_delay:expr,
+        $rect:expr,
+        $dc_blocker:expr,
+        $kaiser:expr,
+        $rnyquist:expr,
+        $notch:expr,
         $destroy:expr)) => {
-        impl $obj {}
+        impl $obj {
+            pub fn reset(&self) {
+                unsafe {
+                    $reset(self.inner)
+                }
+            }
+
+            pub fn print(&self) {
+                unsafe {
+                    $print(self.inner)
+                }
+            }
+
+            pub fn len(&self) -> usize {
+                unsafe {
+                    $glen(self.inner) as _
+                }
+            }
+
+            pub fn freq_response(&self, fc: f32) -> Complex32 {
+                let mut f = Complex32::default();
+                unsafe {
+                    $freq_response(self.inner, fc, f.to_ptr_mut());
+                }
+                f
+            }
+
+            pub fn group_delay(&self, fc: f32) -> f32 {
+                unsafe {
+                    $group_delay(self.inner, fc)
+                }
+            }
+        }
 
         impl Drop for $obj {
             fn drop(&mut self) {
@@ -43,3 +74,4 @@ macro_rules! firfilt_impl {
         }
     };
 }
+
