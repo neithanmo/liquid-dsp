09138590d88cb6f970c20a19782612d397a7135b
neithanmo <neithanmo@gmail.com>
2020-02-04 07:59:50 -0600

    removing impls and using macros

--- src/fft/asgram.rs
+++ src/fft/asgram.rs
@@ -22,7 +22,11 @@ macro_rules! asgram_xxx_impl {
         $create:expr, $reset:expr,
         $setscale:expr, $setdisplay:expr,
         $print:expr,
-        $destroy:expr)) => {
+        $push:expr,
+        $write:expr,
+        $execute:expr,
+        $destroy:expr,
+        $type:ty, $type2:ty)) => {
         impl $obj {
             pub fn create(nfft: u32) -> Result<Self, LiquidError> {
                 if nfft < 2 {
@@ -63,6 +67,38 @@ macro_rules! asgram_xxx_impl {
                     $print(self.inner);
                 }
             }
+
+            pub fn push(&mut self, x: $type2) {
+                unsafe {
+                    $push(self.inner, x.to_c_value());
+                }
+            }
+
+            pub fn write(&mut self, x: &[$type2]) {
+                unsafe {
+                    $write(self.inner, x.to_ptr() as _, x.len() as _);
+                }
+            }
+
+            // TODO: Need a deep check
+            /// compute spectral periodogram output from current buffer contents
+            ///  _ascii      :   output ASCII string [size: _nfft x 1]
+            ///  _peakval    :   value at peak (returned value)
+            ///  _peakfreq   :   frequency at peak (returned value)
+            pub fn execute(&mut self) -> (&str, f32, f32) {
+                let mut peak = 0f32;
+                let mut peakf = 0f32;
+                unsafe {
+                    $execute(
+                        self.inner,
+                        self.ascii.as_mut_ptr() as _,
+                        peak.to_ptr_mut(),
+                        peakf.to_ptr_mut(),
+                    );
+                }
+                let string = str::from_utf8(&self.ascii).unwrap_or(" ");
+                (string, peak, peakf)
+            }
         }
 
         impl Drop for $obj {
@@ -75,72 +111,6 @@ macro_rules! asgram_xxx_impl {
     };
 }
 
-impl AsgramRf {
-    pub fn push(&mut self, x: f32) {
-        unsafe { raw::asgramf_push(self.inner, x) }
-    }
-
-    pub fn write(&mut self, x: &[f32]) {
-        unsafe {
-            raw::asgramf_write(self.inner, x.as_ptr() as _, x.len() as _);
-        }
-    }
-
-    // TODO: Need a deep check
-    /// compute spectral periodogram output from current buffer contents
-    ///  _ascii      :   output ASCII string [size: _nfft x 1]
-    ///  _peakval    :   value at peak (returned value)
-    ///  _peakfreq   :   frequency at peak (returned value)
-    pub fn execute(&mut self) -> (&str, f32, f32) {
-        let mut peak = 0f32;
-        let mut peakf = 0f32;
-        unsafe {
-            raw::asgramf_execute(
-                self.inner,
-                self.ascii.as_mut_ptr() as _,
-                peak.to_ptr_mut(),
-                peakf.to_ptr_mut(),
-            );
-        }
-        let string = str::from_utf8(&self.ascii).unwrap_or(" ");
-        (string, peak, peakf)
-    }
-}
-
-impl AsgramCf {
-    pub fn push(&mut self, x: Complex32) {
-        unsafe {
-            raw::asgramcf_push(self.inner, x.to_c_value());
-        }
-    }
-
-    pub fn write(&mut self, x: &[Complex32]) {
-        unsafe {
-            raw::asgramcf_write(self.inner, x.to_ptr() as _, x.len() as _);
-        }
-    }
-
-    // TODO: Need a deep check
-    /// compute spectral periodogram output from current buffer contents
-    ///  _ascii      :   output ASCII string [size: _nfft x 1]
-    ///  _peakval    :   value at peak (returned value)
-    ///  _peakfreq   :   frequency at peak (returned value)
-    pub fn execute(&mut self) -> (&str, f32, f32) {
-        let mut peak = 0f32;
-        let mut peakf = 0f32;
-        unsafe {
-            raw::asgramcf_execute(
-                self.inner,
-                self.ascii.as_mut_ptr() as _,
-                peak.to_ptr_mut(),
-                peakf.to_ptr_mut(),
-            );
-        }
-        let string = str::from_utf8(&self.ascii).unwrap_or(" ");
-        (string, peak, peakf)
-    }
-}
-
 asgram_xxx_impl!(
     AsgramCf,
     (
@@ -149,7 +119,11 @@ asgram_xxx_impl!(
         raw::asgramcf_set_scale,
         raw::asgramcf_set_display,
         raw::asgramcf_print,
-        raw::asgramcf_destroy
+        raw::asgramcf_push,
+        raw::asgramcf_write,
+        raw::asgramcf_execute,
+        raw::asgramcf_destroy,
+        f32, Complex32
     )
 );
 
@@ -161,7 +135,11 @@ asgram_xxx_impl!(
         raw::asgramf_set_scale,
         raw::asgramf_set_display,
         raw::asgramf_print,
-        raw::asgramf_destroy
+        raw::asgramf_push,
+        raw::asgramf_write,
+        raw::asgramf_execute,
+        raw::asgramf_destroy,
+        f32, f32
     )
 );
 
