8f97d59cefe8336e633b74f83bfb6eb15f670a2e
neithanmo <neithanmo@gmail.com>
2020-02-05 13:35:36 -0600

    handle possible erros caused by invalid params

--- src/cvsd.rs
+++ src/cvsd.rs
@@ -2,7 +2,9 @@ use libc::c_uint;
 use std::fmt;
 
 use crate::liquid_dsp_sys as raw;
-
+use crate::errors::LiquidError;
+use crate::LiquidResult;
+/// CVSD: continuously variable slope delta
 pub struct Cvsd {
     inner: raw::cvsd,
     num_bits: u32,
@@ -11,21 +13,42 @@ pub struct Cvsd {
 }
 
 impl Cvsd {
-    pub fn create(num_bits: u32, zeta: f32, alpha: f32) -> Self {
+
+    /// create cvsd object
+    ///  num_bits   :   number of adjacent bits to observe
+    ///  zeta       :   slope adjustment multiplier
+    ///  alpha      :   pre-/post-emphasis filter coefficient (0.9 recommended)
+    /// NOTE: _alpha must be in [0,1]
+    pub fn create(num_bits: u32, zeta: f32, alpha: f32) -> LiquidResult<Self> {
+        if num_bits == 0 {
+            return Err(LiquidError::InvalidValue(
+                "num_bits must be positive".to_owned(),
+            ));
+        } else if zeta <= 1f32 {
+            return Err(LiquidError::InvalidValue(
+                "zeta must be greater than 1".to_owned(),
+            ));
+        } else if alpha < 0f32 || alpha > 1f32 {
+            return Err(LiquidError::InvalidValue(
+                "alpha must be in [0,1]".to_owned(),
+            ));
+        }
         unsafe {
-            Self {
+            Ok(Self {
                 inner: raw::cvsd_create(num_bits as c_uint, zeta, alpha),
                 num_bits,
-                zeta,
                 alpha,
-            }
+                zeta,
+            })
         }
     }
 
+    /// encode single sample
     pub fn encode(&self, audio_sample: f32) -> u8 {
         unsafe { raw::cvsd_encode(self.inner, audio_sample) as u8 }
     }
 
+    /// decode single sample
     pub fn decode(&self, bit: u8) -> f32 {
         unsafe { raw::cvsd_decode(self.inner, bit) }
     }
